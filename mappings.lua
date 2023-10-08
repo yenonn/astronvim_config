5fd050d4604b941b468bd9fa72ebebb0dec06b78
yenonn <yenonn@gmail.com>
2023-10-08 21:12:23 +0800

    WIP

--- mappings.lua
+++ mappings.lua
@@ -33,6 +33,8 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     ["<leader>l"] = {":Lazy check<cr>", desc = "Lazy check"},
+    ["<S-h>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer"},
+    ["<S-l>"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,desc = "Previous buffer"},
   },
   t = {
     -- setting a mapping to false will disable it
